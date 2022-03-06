c10dad3c73538dd848e8c97738194cc6f1182952
vKaliWin <vKali@vkali.exe>
2022-03-06 05:00:48 -1000

    .

--- 0_Udem/C_44/21_arreglos_con_listas_y_tablas/03_caracter_y_string.c
+++ 0_Udem/C_44/21_arreglos_con_listas_y_tablas/03_caracter_y_string.c
@@ -4,12 +4,12 @@ int main(){
                   // 16 espacios para 1i6 caracteres, todo final de linea lleva un extra '\0' 
   char cadena[16]="estaEs unaCadena";   // le doy una posici[on extra para el '\0', si le doy espacioExacto le imprime una 'G' al final de linea :(
   char arrayDes[16]={'e','s','t','o',' ','E','s',' ','u','n','a',' ','a','r','r','y'};// le doy una posici[on extra para el '\0', si le doy espacioExacto le imprime una 'G' al final de linea :(
-/*
+
   printf("cadenaString=> %s\n",cadena);
   printf("\n----------------------\n");
   printf("arrayArreglo=> %s\n",arrayDes);
   printf("\n----------------------\n");
-*/
+
   for(int i=0; i<16; i++){
     printf("%c",arrayDes[i]);
   }
