918bfaf6dc955ae9075d9ef5aa3278b4c074ed16
vKaliWin <vKali@vkali.exe>
2022-04-21 02:11:23 -1000

    .

--- 0_Udem/C_44/25__ejer_struct/02_typedef_struct__structFuncionParametro.c
+++ 0_Udem/C_44/25__ejer_struct/02_typedef_struct__structFuncionParametro.c
@@ -25,56 +25,99 @@ int main(void){
 
 
 
-  printf("\nEste programa permite ver las materias, Modificarlas y Cread alumnos\n");
-  printf("\n1) Ver Materias\n2) Modificar Materias\n3) Crear Alumnos desde Cero con sus Materias\n0) Salir...\n");
+  int a=1;
+  while(a==1){
+    printf("\n\n\n\tEste programa permite ver las materias, Modificarlas y Crear 1 alumno\n");
+    printf("\n1) Ver Materias\n2) Modificar Materias\n3) Alumno Nuevo(solo 1) \n0) Salir...\n");
 
-  Alumno Nuevo;
-  char alumnoNuevo[20];
+    Alumno Nuevo;
+    char alumnoNuevo[20];
 
-  int user_option;
-  scanf("%d",&user_option); getchar();
-  switch(user_option){
-    case 1:
-      printf("\n\t SELECCIONASTE VER MATERIAS...");
-      printf("\nDe que alumno quieres ver las Materias?:");
-      printf("  1) Manolo\t 2) Manuela\t 3) %s",alumnoNuevo);  // alumnoNuevo no declarado
+    int user_option;
+    scanf("%d",&user_option); getchar();
+    switch(user_option){
+      case 1:
+        printf("\n\t ::: SELECCIONASTE VER MATERIAS...");
+        printf("\nDe que alumno quieres ver las Materias?:");
+        printf("\n\t1) Manolo\t 2) Manuela\t 3) %s\n\t\t::: ",alumnoNuevo);  // alumnoNuevo no declarado
 
-      int alumno;
-      scanf("%d",&alumno); getchar();
-      switch(alumno){
-        case 1:
-          printf("\nSeleccionaste a Manolo: ");
-          printf("\nSus materias son: \n\t1) %s\n\t2) %s\n\t3) %s\n", Manolo.goro[0].Materia, Manolo.goro[1].Materia, Manolo.goro[2].Materia);
-          break;
-        case 2:
-          printf("\nSeleccionaste a Manuela: ");
-          printf("\nSus materias son: \n\t1) %s\n\t2) %s\n\t3) %s\n ",Manuela.goro[0].Materia, Manuela.goro[1].Materia, Manuela.goro[2].Materia);
-          break;
-        case 3:
-          break;
-        default: system("clear"); printf("\n Opci[on incorrecta...!!!\n"); break;
-      }
-      break;
-    case 2:
-      printf("\n\t MODIFICAR MATERIAS...\n");
-      printf("\n Modificar las materias de: \n");
-      printf("\t1) Manolo\t 2) Manuela\n");
+        int alumno;
+        scanf("%d",&alumno); getchar();
+        switch(alumno){
+          case 1:
+            system("clear");
+            printf("\nSeleccionaste a Manolo: ");
+            printf("\nSus materias son: \n\t1) %s\n\t2) %s\n\t3) %s\n", Manolo.goro[0].Materia, Manolo.goro[1].Materia, Manolo.goro[2].Materia);
+            break;
+          case 2:
+            system("clear");
+            printf("\nSeleccionaste a Manuela: ");
+            printf("\nSus materias son: \n\t1) %s\n\t2) %s\n\t3) %s\n ",Manuela.goro[0].Materia, Manuela.goro[1].Materia, Manuela.goro[2].Materia);
+            break;
+          case 3:
+            system("clear");
+            printf("\nSeleccionaste a %s: ", alumnoNuevo);
+            printf("\nSus materias son: \n\t1) %s\n\t2) %s\n\t3) %s\n ",Nuevo.goro[0].Materia, Nuevo.goro[1].Materia, Nuevo.goro[2].Materia);
+            break;
+          default: system("clear"); printf("\n Opci[on incorrecta...!!!\n"); break;
+        }
+        break;
+      case 2:
+        printf("\n\t MODIFICAR MATERIAS...\n");
+        printf("\n Modificar las materias de: \n");
+        printf("\t1) Manolo\t 2) Manuela\t3) %s\t\t0) Back\n",alumnoNuevo);
 
-      int user_modificacion;
-      scanf("%d",&user_modificacion); getchar();
+        int user_modificacion;
+        scanf("%d",&user_modificacion); getchar();
 
-      switch(user_modificacion){
-        case 1:
+        switch(user_modificacion){
+          case 1:
+            printf("\n\t #=>> Manolo\n"); 
+            for(int i=0; i<3; i++){
+              printf("\nIngrese la materia %d: ",i+1);
+              fgets(Manolo.goro[i].Materia, 40, stdin); strtok(Manolo.goro[i].Materia, "\n");
+            }
+            for(int i=0; i<3; i++){
+              printf("\n#=> %s",Manolo.goro[i].Materia);
+            }
+            break;
+          case 2:
+            printf("\n\t #=>> Manuela\n"); 
+            for(int i=0; i<3; i++){
+              printf("\nIngrese la materia %d: ",i+1);
+              fgets(Manuela.goro[i].Materia, 40, stdin); strtok(Manuela.goro[i].Materia, "\n");
+            }
+            for(int i=0; i<3; i++){
+              printf("\n#=> %s",Manuela.goro[i].Materia);
+            }
+            break;
+          case 3:
+            printf("\n\t #=>> %s\n", alumnoNuevo); 
+            for(int i=0; i<3; i++){
+              printf("\nIngrese la materia %d: ",i+1);
+              fgets(Nuevo.goro[i].Materia, 40, stdin); strtok(Nuevo.goro[i].Materia, "\n");
+            }
+            for(int i=0; i<3; i++){
+              printf("\n#=> %s",Nuevo.goro[i].Materia);
+            }
+            break;
+          case 0: break;
+          default: system("clear"); printf("\n Opci[on incorrecta...!!!\n") ;break;
+        }
+        break;
+      case 3:
+        system("clear");
+        printf("\n Nombre nuevo alumno:  ");
+        fgets(alumnoNuevo, 20, stdin); strtok(alumnoNuevo, "\n");
 
-          break;
-        case 2:
-          break;
-        default: system("clear"); printf("\n Opci[on incorrecta...!!!\n") ;break;
-      }
-      break;
-    case 3:
-      break;
-    default: system("clear"); printf("\n Opci[on incorrecta...!!!\n"); break;
+        for(int i=0; i<3; i++){
+          printf("\nIngrese la materia %d: ",i);
+          fgets(Nuevo.goro[i].Materia, 40, stdin); strtok(Nuevo.goro[i].Materia, "\n");
+        }
+        break;
+      case 0: system("clear"); a=2; printf("\n\n\tSaliendo...\n"); break;
+      default: system("clear"); printf("\n Opci[on incorrecta...!!!\n"); break;
+    }
   }
 
   return 0;
