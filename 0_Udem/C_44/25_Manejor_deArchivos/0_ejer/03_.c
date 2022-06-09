5085ec796519ace0374810dd2c06b4c917fbf2a3
vKaliWin <vKali@vkali.exe>
2022-06-09 04:06:46 -1000

    .

---
+++ 0_Udem/C_44/25_Manejor_deArchivos/0_ejer/03_.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+//char direccion[] = "/home/user_Here/Desktop/Folder/Folder/FOlder/Folder/0_ejer";
+  char rpt;
+
+  FILE *apuntador_Archivo;
+  apuntador_Archivo = fopen("03_.txt", "at");
+
+
+
+  return 0;
+}
