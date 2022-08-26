7145d441f4f4893e104ec7bd41db3dcda73dfe6a
localHost <local@host.exe>
2022-08-25 22:38:23 -0400

    .

--- if_likeRuby_oneLine.c
+++ if_likeRuby_oneLine.c
@@ -7,5 +7,6 @@ int main(void){
   (1 == 1)? printf("\nif 1\n"), printf("if 2 COMA\n") : printf("else\n"), printf("else2\n\n");
 
   (2 == 1)? ( printf("\nif 1\n"), printf("if 2 COMA\n") ) : ( printf("else\n"), printf("else2\n") );
+
   return 0;
 }
