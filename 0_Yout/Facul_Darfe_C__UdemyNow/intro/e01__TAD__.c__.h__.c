c528114399e75120b2bc09bbc54201b91486408c
mK <local@host.exe>
2023-03-13 03:30:17 -0500

    .

---
+++ 0_Yout/Facul_Darfe_C__UdemyNow/intro/e01__TAD__.c__.h__.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "e02.h"
+
+   //    ::: gcc  -o  e04_test  e0*.c
+
+int main(int argc, char *argv[]){
+
+  little_boy();
+
+  return 0;
+}
---
+++ 0_Yout/Facul_Darfe_C__UdemyNow/intro/e02.h
@@ -0,0 +1,8 @@
+#ifndef E02_H
+#define E02_H
+
+   //    ::: gcc  -o  e04_test  e0*.c
+
+void little_boy();
+
+#endif
