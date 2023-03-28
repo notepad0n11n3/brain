36f2e7d0217652dce37ea10788377200ba8872cd
mK <local@host.exe>
2023-03-27 23:49:22 -0500

    .

---
+++ 0_Yout/ensamblador_x86/a01.asm
@@ -0,0 +1,19 @@
+section .data
+
+msg db "Welcome Back!!!",0xA,0xD
+len equ $ - msg
+
+section .text
+  global _start
+
+_start:
+  mov eax, 4      ;llama al sistema (sys_write)
+  ;eax = 4
+
+  mov ebx, 1      ;stdout
+  mov ecx, msg    ;msg pantalla
+  mov edx, len    ;longitud del mensaje
+  int 0x80        ;llamada al sistema de interrupciones
+
+  mov eax, 1      ;(  system("pause") , exit , sys_exit  )
+  int 0x80
