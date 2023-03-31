f0bf44475b52e68e8b3da46dd0f0f9b3ec8a8d56
mK <local@host.exe>
2023-03-31 12:01:01 -0500

    .

---
+++ 0_Yout/JognHammond/hello_world.asm
@@ -0,0 +1,31 @@
+; hellow world.asm
+
+; XD,   esto esta mucho mejor.
+  ;     ::: python3
+  ;   python3:: 0xa  
+  ;   python3:: 0xb    
+
+  ; locate unistd_32.h         ('asm... <- <3 <3 <3') .... <3 <3 <3 ... ! ! !
+
+global _start
+
+section .text:
+_start:
+ mov eax, 0x4             ; use the write syscall 
+ mov ebx, 1               ; use stdout as the fd `$(man 2 write)`
+ mov ecx, message         ; use the message as the buffer
+ mov edx, message_length  ; and supply the length
+ int 0x80                 ; invoker the syscall
+
+ ; now gracefully exit
+ mov eax, 0x1
+ mov ebx, 0
+ int 0x80 
+
+section .data:
+  message: db "Hello world!",0xA
+  message_length: equ $-message
+
+  mensaje2 db "segundo hello...",0xA
+  mensaje2_length equ $ - mensaje2
+
