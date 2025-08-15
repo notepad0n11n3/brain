03ce96540d9e4cf9dc593259f77a6f5c5f82a45c
vk_K <local@host.exe>
2025-08-15 15:52:12 -0500

    .

---
+++ 0_Udem/C_dominaC_FacundoDarfe/a01_declaracionVariables.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+
+	// Declaracion de varialbes
+	short a;		//entero mas peque~o que int, especial para edades etc.
+	int a;
+	float real;
+	double realDoble;
+	unsigned char c;		// sin signo negativo (-)
+	unsigned short s;		// sin signo negativo (-)
+	long long int enteroLargoLargo;
+	long double doblePrecision;
+	unsigned long long int sinSignoEnteroLargo;
+	
+	short edad = 10000;
+	long int edad = 1000000000000;
+	long long int edad = 100000000000000000000000;
+	long double edad = 1000000000000000000000000000000;
+
+	return 0;
+}
